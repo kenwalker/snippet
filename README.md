3afcafdf4a34cdee922a5ef10f3ef4c90b4afb8c
kenwalker <ken_walker@ca.ibm.com>
2013-02-19 22:45:55 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 snippet
 =======
 
-An Orion Plugin to generate a blog post insert from selected code snippet
+This is an Eclipse Orion plugin to create an HTML Snippet that will display the selected text in an editor.
+
+After installing the plugin there will be a new button in the text editor labelled "Snippet". Select some text editor and press it. A new section of text will be inserted into the editor containing an entire HTML Snippet that when inserted into your blog should display the selected text in a read-only version of the Orion Editor.
+
+Copy or Cut the text into the clipboard and paste into your raw HTML blog post.
+
+Here's an example Snippet generated from the Snippet plugin
---
+++ Snippet.js
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors:
+ *    Ken Walker
+ *******************************************************************************/
+
+/*global define eclipse window document */
+window.onload = function() {
+	var scriptString = '<link rel="stylesheet" type="text/css" href="http://eclipse.org/orion/editor/releases/2.0/built-editor.css"/>\n<script src="http://eclipse.org/orion/editor/releases/2.0/built-editor.min.js">\n</script>\n<script>\n\trequire(["orion/editor/edit"], function(edit) {\n\t\tedit({className: "editor"});\n\t});\n</script>';
+	var preCode = '<pre class="editor" data-editor-lang="js" data-editor-readonly="true">';
+	var postCode = '</pre>';
+	
+	// create the plugin
+	var headers = {
+		name: "Code Snippet Editor Plugin",
+		version: "0.1",
+		description: "Creates an embedded text widget from code selection in the Editor"
+	};
+	
+    var provider = new orion.PluginProvider(headers);
+    var serviceImpl = {
+        run: function(selectedText, text, selection) {
+            var codeSnippet;
+            var selectionEmpty = selection.start === selection.end;
+            if (selectionEmpty) {
+                codeSnippet = text;
+            } else {
+                codeSnippet = selectedText;
+            }
+            var div = document.createElement("div");
+            div.textContent = codeSnippet;
+            var theCode = div.innerHTML;
+            var result = scriptString + '\n' + preCode + '\n' + theCode + '\n' + postCode + '\n';
+            return result;
+        }
+    };
+
+    var serviceProperties = {
+        name: 'Snippet',
+        tooltip: 'Create a JavaScript Orion Editor Snippet for a Web Page',
+        key: ['S', true, true] // Ctrl+Shift+D
+    };
+    provider.registerServiceProvider('orion.edit.command', serviceImpl, serviceProperties);
+    provider.connect();
+};
